b055afbf8b4af3dedb34ef315a08c6aa62471519
RongJhen-Li <10756022@ntub.edu.tw>
2021-12-20 11:37:17 +0800

    feat(coffees): add add page

--- coffees/views.py
+++ coffees/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.shortcuts import render, get_object_or_404, redirect
 
+from .forms import CoffeeForm
 from .models import Coffee
 
 
@@ -13,3 +15,12 @@ def index(request):
 def show(request, pk):
     coffee = get_object_or_404(Coffee, pk=pk)
     return render(request, 'coffees/show.html', {'coffee': coffee})
+
+
+def add(request):
+    form = CoffeeForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        messages.success(request, '新增成功')
+        return redirect('coffees:index')
+    return render(request, 'coffees/add.html', {'form': form})
